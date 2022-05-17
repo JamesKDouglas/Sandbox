4c20bce104a34764219d7e331391b4ea5d6fb4d4
James Douglas <james@phytochemconsulting.ca>
2022-05-17 09:16:53 -0700

    add How Much Water

--- Baum-SweetSequence/js/main.js
+++ Baum-SweetSequence/js/main.js
@@ -8,20 +8,47 @@
 //11001 does not (group of 2 zeros)
 //11101 does belong (group of 1 zeros)
 //Pseudocode:
+//A for loop goes through the numbers 0 to 1 million
+//it sends the next baum sweet each time it is run - it doesn't just dump them all out. hence being a generator
+// 
 
-// function* baumSweet() {
-//     yield 1;
-//   }
-
-function* generator(i) {
-  yield i;
-  yield i + 10;
-  yield i + 12;
+function* baumSweet() {
+  while (i<10){
+    i++;
+    yield i;
+  } 
 }
 
-const gen = generator(10);
+function* generator() {
+  while (b<10){
+    yield b.toString(2);
+    b++;
+  }
+}
 
-console.log(gen.next().value);
-// expected output: 10
+// console.log(generator());
 
-console.log(gen.next().value);
+// console.log(generator());
+
+function convertToBinary(x) {
+  let bin = 0;
+  let rem;
+  let i = 1;
+  let step = 1;
+
+  while (x != 0) {
+      rem = x % 2;
+      console.log(
+          `Step ${step++}: ${x}/2, Remainder = ${rem}, Quotient = ${parseInt(x/2)}`
+      );
+      x = parseInt(x / 2);
+      bin = bin + rem * i;
+      i = i * 10;
+  }
+  return bin;
+}
+
+// take input
+//let number = prompt('Enter a decimal number: ');
+
+//convertToBinary(number);
