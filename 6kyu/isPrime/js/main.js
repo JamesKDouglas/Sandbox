c9a634b40149d92d9e324ad081cf9affe12d9c1b
James Douglas <james@phytochemconsulting.ca>
2022-07-27 11:07:51 -0700

    solved isPrime by reducing the factor pool to check from both ends

--- 6kyu/isPrime/js/main.js
+++ 6kyu/isPrime/js/main.js
@@ -26,26 +26,55 @@
 // First is two of course.
 // then... well you can't really skip anything.  
  
+// function isPrime(num) {
+
+//     //early returns
+//     if (num<=1){
+//         return false;
+//     }
+//     if (num%10 == 5){
+//         return false;
+//     }
+
+//     // Main bit
+
+//     for(let i = 2;i<=num/2;i++){
+//         if (num%i ==0){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// still times out. This should take about 40% of the time but anyways it is still too slow
+
+
+//this works fast enough, completing in 219ms.:
 function isPrime(num) {
-
+    console.log(`number: ${num}`);
     //early returns
-    if (num<=1){
+    if (num<=1){//negative numbers get rejected. They are not prime since they all have factor -1. Zero and one are also not considered prime.
+        console.log(`number is less than or equal to one - not prime.`)
         return false;
-    }
-    if (num%10 == 5){
-        return false;
+    } else if (num==2){
+        console.log(`number is 2 - prime`);
+        return true;
     }
 
-    // Main bit
+    //MaxCheck algorithm: We work only with integers. If a number is indivisible by, for ex. up to 5, there is no point in checking beyond n/5 since the paired factor would have to be smaller than n/5 to make n of course. So the potential factor pool shrinks from both ends. 
 
-    for(let i = 2;i<=num/2;i++){
+    let maxCheck = 0;
+
+    for(let i = 2;i<=Math.ceil(num/maxCheck);i++){
+        //console.log(`checking factor ${i}`)
         if (num%i ==0){
             return false;
         }
+        maxCheck = i;
     }
     return true;
 }
-// still times out. This should take about 40% of the time but anyways it is still too slow
-let n = 13;
+let n = 4;
 console.log(isPrime(n));
+
+//I see others setting the upper limit as the square root, which also makes sense.
 
