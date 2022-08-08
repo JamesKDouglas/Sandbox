80e61d5e4aba352abe9fe880827c4fc21df43f34
DouglasKJames <douglas.k.james@gmail.com>
2022-08-08 15:12:34 -0700

    compete GetCharFromAscii completely with phone!

---
+++ GetCharFromAscii/js/main.js
@@ -0,0 +1,9 @@
+//parameters An integer representing an ascii character
+//return The ascii character, as a string.
+//example the code 55 should return the string "7"
+//pseudocode:
+//find a method in js which can do this.
+
+function getChar(c){
+  return String.fromCharCode(c);
+}
