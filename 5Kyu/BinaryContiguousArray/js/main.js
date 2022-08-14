789c356b8bb6987ba75cc07c9b1d4a59d1c7c3d0
James Douglas <james@phytochemconsulting.ca>
2022-08-14 12:47:19 -0700

    add some runtime analysis to binaryContiguousArray which proves the code is just fine. Something is wrong with the final test case by CodeWars.

--- 5Kyu/BinaryContiguousArray/js/main.js
+++ 5Kyu/BinaryContiguousArray/js/main.js
@@ -3,12 +3,37 @@
 //in dynamic programming you take the data you want to examine then generate some sort of like meta data.
 //so first, go through the array and generate a second one. Score 1 for a 1 and -1 for a zero.
 //then scan from the maximum downwards (or min upwards). When multiple X values are found for a given Y then take the min, max and calculate the span. test to see if it is larger than the old span. If it is, rewrite the old span length.
+
+//try to put some data into local storage so I can use the same set multiple times.
+
+function genData(){//To test for timing stuff I need a largish data set.
+    let arr = [];
+    let digit = 0;
+    for(let i=0;i<120000;i++){//I'm getting a span reported of 122569 even though I only generate 120 kb sets. I think somehow it's appending the data to the existing dataset each time it runs. The dataset is double the size that it should be, indicated by running .length;
+        digit = Math.round(Math.random());
+        arr.push(digit);
+    }
+    localStorage.setItem('a dataset', arr);
+    console.log(`robot makes a dataset!`);
+    return (`robot makes a dataset!`);
+}
+function printRT(start, comment){
+    //timestamp to console.
+    console.log(start);
+    let elapsedTime = Date.now() - start;
+    console.log(`runtime ${comment}: ${elapsedTime}`);
+}
+
 function binarray(a){
     let span = 0;
     let chartArr = [0];
     let yHeight = 0;
 
-    //make the xy chart
+    const startTime = Date.now();
+
+    printRT(startTime, "before xychart");
+
+     //make the xy chart
     for (let i=0;i<a.length;i++){
         if(a[i]==0){
             yHeight--;
@@ -19,9 +44,12 @@ function binarray(a){
         chartArr.push(yHeight);
     }
 
+    //timestamp
+    printRT(startTime, "after xychart, before scan");
+
     //now do the scan
     // let max = Math.max(...chartArr);//This uses the apply method only works with small arrays. Reduce doesn't have that issue so lets use that here.
-    
+
     //start at the top
     let max = chartArr.reduce((a,b) => Math.max(a,b), -Infinity);
     // console.log(max);
@@ -33,6 +61,8 @@ function binarray(a){
         //Imagine a line scans downwards. When the horizontal line intersects the xychart line it intersects with it once, twice or more. Record all those x values and put them in to an array called indexes.
  
         let indexes = [];
+
+        //This is too slow?
         for(let j = 0; j < chartArr.length; j++){
             // console.log(`building indexes array. Line is at ${i}, examining element ${j}`);
             if (chartArr[j] === i){
@@ -51,13 +81,29 @@ function binarray(a){
             span = newSpan;
         }
     }
+
+    
+    printRT(startTime, "after scan and span calc");
+    console.log(`span: ${span}`);
     return span;
 }
 
 
-a=[0,0,1,1,1,0,0,0,0,0];
+// a=[
+//     1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0,
+//     1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+//     1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0,
+//     1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0,
+//     0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1,
+//     1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+//     1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1,
+//     0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1,
+//     1, 0, 1, 1
+//   ];
 
-console.log(binArray(a));
+// to get a from local storage in the console
+// localStorage.getItem("a dataset");
+console.log(`span: ${binarray(a)}`);
 
 //These are examples, with the value at the end the expected one
 // [[[0,1], 2],
@@ -67,3 +113,17 @@ console.log(binArray(a));
 // [[0,0,1,1,1,0,0,0,0,0], 6]]
 
 // This does work. However, again it is too slow and times out. How can I make it faster?
+
+//Ok my 2013 macbook pro runs this program in between 300 and 600 ms for a dataset with 240 000 elements. 
+//CodeWars is taking over 12 000 ms to run it. 
+
+// binarray(localStorage.getItem("a dataset"));
+// 12:35:11.801 main.js:22 1660505711801
+// 12:35:11.801 main.js:24 runtime before xychart: 0
+// 12:35:11.852 main.js:22 1660505711801
+// 12:35:11.852 main.js:24 runtime after xychart, before scan: 51
+// 12:35:12.407 main.js:22 1660505711801
+// 12:35:12.409 main.js:24 runtime after scan and span calc: 608
+// 12:35:12.417 122569
+
+//By putting logging into the codewars run code (copying this) I can see that the spans returned at 100 000 in size are takin about 100ms to run the code. So then why does a "very big" 120 000 crash it?
