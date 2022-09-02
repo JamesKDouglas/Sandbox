f5c7a3bf45a78f86dcca1b94cddbee1a873e8d3f
James Douglas <james@phytochemconsulting.ca>
2022-09-02 12:12:22 -0700

    finish rot90Clock for MovesInSquaredStringsIII

--- 6kyu/MovesInSquaredStringsII/js/main.js
+++ 6kyu/MovesInSquaredStringsII/js/main.js
@@ -1,48 +1,14 @@
-let s = "abcd\nefgh\nijkl\nmnop";
+function rot(s) {
+    return s.split("").reverse().join("");
+  }
+  
+  function selfieAndRot(s) {
+    return (s = s.replace(/.+/g, t => t + t.replace(/./g, "."))) + "\n" + rot(s);
+  }
+  
+  function oper(fn, s) {
+    return fn(s);
+  }
 
-function rot(strng){
-    let lines = strng.split(`\n`);
-    for (let i=0;i<lines[0].length;i++){
-        lines[i] = lines[i].split('').reverse().join("");
-
-    }   
-    return lines.reverse().join("\n");
-}
-
-function selfieAndRot(strng){
-
-    //first half
-    let lines = strng.split(`\n`);
-    let tempString = "";
-    let period = ".";
-    let linesP = [];//for some reason I require this new array, i am unable to assign to the previous
-    for (let i=0;i<lines[0].length;i++){
-        tempString = lines[i].concat(period.repeat(lines[0].length));
-        linesP[i] = tempString;
-        tempString = "";
-    }   
-    let firstHalf = linesP.join("\n");
-
-    firstHalf = firstHalf.concat("\n");
-
-    //second half
-    linesP = [];
-    for (let i=0;i<lines[0].length;i++){
-        lines[i] = lines[i].split('').reverse().join("");
-        tempString = tempString.concat("....", lines[i]);
-        linesP[i] = tempString;
-        tempString = "";
-    }   
-    let secondHalf = linesP.reverse().join("\n");
-    return firstHalf + secondHalf;
-}
-
-function testFunc(s){
-    console.log(s);
-}
-function oper(fct, s) {
-    return fct(s);
-}
-// console.log(rot(s));
-// console.log(oper(selfie_and_rot,s));
-console.log(oper(selfieAndRot,s));
+  let s = "abcd\nefgh\nijkl\nmnop";
+  console.log(oper(selfieAndRot,s));
