0f857c3b908cea60ae03ee3acf2961542e441ff2
James Douglas <james@phytochemconsulting.ca>
2022-10-05 09:42:11 -0700

    add ConvertBooleanValuesToStrings

---
+++ ConvertBooleanValuesToStrings/js/main.js
@@ -0,0 +1,16 @@
+// parameters: Input a boolean.
+// return: Yes or No, Yes for if the boolean is true and No for if it is false.
+// example: Not really needed, this is so simple!
+// pseudocode:
+//Just an if statement will do fine.
+
+function boolToWord( bool ){
+    if (bool){
+      return "Yes";
+    } else {
+      return "No";
+    }
+  }
+
+  //The reason I'm doing this extremely easy codewars today is because I need all the time I can get to perform an experiment to determine the time complexity of string methods.
+
