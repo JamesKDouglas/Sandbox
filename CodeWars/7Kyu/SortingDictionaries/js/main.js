a62b2251b7a9b0fa272ef0e5c685b39336d37601
James Douglas <james@phytochemconsulting.ca>
2022-10-17 09:27:56 -0700

    add SortingDictionaries correction

--- CodeWars/7Kyu/SortingDictionaries/js/main.js
+++ CodeWars/7Kyu/SortingDictionaries/js/main.js
@@ -14,18 +14,21 @@
 
 function sortDict(dict) {
     let arr = [];
-    for (el in dict){//could use spread operator
-        arr.push([el[0], el[1]]);
+    let keys = Object.keys(dict);
+    // console.log(keys);
+    for (k of keys){//could use spread operator
+        // console.log(k);
+        arr.push([+k, dict[k]]);
     }
 
-    arr.sort((a,b) => a[1]>b[1] ? 1 : 0);
+    arr.sort((a,b) => b[1]-a[1]);
 
     return arr;
 
 }
 
-console.log(sortDict({3:1, 2:2, 1:3}, [[1,3], [2,2], [3,1]]));
+console.log(sortDict({3:1, 2:2, 1:3}), [[1,3], [2,2], [3,1]]);
 
-console.log(sortDict({1:2, 2:4, 3:6}, [[3,6], [2,4], [1,2]]));
+console.log(sortDict({1:2, 2:4, 3:6}), [[3,6], [2,4], [1,2]]);
 
-console.log(sortDict({3:1, 2:2, 1:3}, [[1,3], [2,2], [3,1]]));
+console.log(sortDict({3:1, 2:2, 1:3}), [[1,3], [2,2], [3,1]]);
