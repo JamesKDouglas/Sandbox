07e5ca23953a30f37f491bda380fbbcca0e50101
James Douglas <james@phytochemconsulting.ca>
2023-03-26 12:02:52 -0700

    finish DecimalTime

--- CodeWars/DecimalTime/js/main.js
+++ CodeWars/DecimalTime/js/main.js
@@ -2,12 +2,14 @@
 
 //One (toIndustrial) takes an input of time elapsed using a time base
 //of 60 minutes in an hour.
-//The input is minutes as an inteer Or a string in
-// format "h:mm" ex 1:30 for 1 and a half hours.
+//The input is minutes as an integer or a string in
+// format "h:mm" ex "1:30" for 1 and a half hours.
+// OR 90 minutes
 
 //The goal is to convert the time to a base that uses 100 minutes in an hour.
 //and 36 seconds in a minute. So seconds are the same length. 
 // output will be a decimal in 100 minute/hour time. Round to 2 decimal precision.
+//ex: 1:30 or 90 in, 1.50 out
 
 //Two (toNormal) will have an input that is a number type.
 //the time base will be assume as in 100 minutes/hr for the input.
@@ -24,31 +26,45 @@
 
 
 function toIndustrial(time){
-    //Is the input a string or number?
-    //if and typeof for that.
-    //If it's a string, I'll have to parse it to a number.
-    //If it's a number, just divide by 60 round and truncate then return.
-    
-    let newArr = [];
-    if (typeof(time) === "number"){
-      return Math.round((time/60)*100)/100;
-    } else {
-      newArr = time.split(":");
-      return Number(newArr[0]) + Math.round((Number(newArr[1])/60)*100)/100;
-    }
-    
+  //Is the input a string or number?
+  //if and typeof for that.
+  //If it's a string, I'll have to parse it to a number.
+  //If it's a number, just divide by 60 round and truncate then return.
+  
+  let newArr = [];
+  if (typeof(time) === "number"){
+    return Math.round((time/60)*100)/100;
+  } else {
+    newArr = time.split(":");
+    return Number(newArr[0]) + Math.round((Number(newArr[1])/60)*100)/100;
   }
   
-  function toNormal(time){
-    let hrs = time.toString().split(".")[0];
-    let mins = Math.round(Number(time.toString().split(".")[1])*60/100);
-    console.log(mins);
-  
+}
+
+function toNormal(time){
+//   console.log(time);
     
-    let timeStr = `${hrs}:${mins.toString()}`;
-    return timeStr;
+  let hrs = Math.floor(time);
+  
+//   console.log(hrs);
+
+  //We have to make sure zeros get dropped. 1.5 can't end up the same as 1.05.
+  
+  let mins = (time%1)*60;//extract the decimal and find out how many minutes
+  mins = mins.toFixed(0);//same as math.round
+  
+//   console.log(mins);
+
+  if (mins<10){//pad with leading 0
+    mins = "0"+mins.toString();
+  } else {
+    mins = mins.toString();
   }
   
-  console.log(toIndustrial(1), 0.02);
-  console.log(toIndustrial("1:45"), 1.75);
-  console.log(toNormal(0.33), "0:20");
+  let timeStr = `${hrs.toString()}:${mins}`;
+  return timeStr;
+}
+
+console.log(toIndustrial(1), 0.02);
+console.log(toIndustrial("1:45"), 1.75);
+console.log(toNormal(0.33), "0:20");
