f7cb852ec3c590b2a34405b06cc4bbca1b0b45e1
James Douglas <james@phytochemconsulting.ca>
2023-04-07 16:51:57 -0700

    add GrasshopperTerminalGame1

---
+++ CodeWars/GrasshopperTerminalGame1/js/main.js
@@ -0,0 +1,10 @@
+
+function Hero (name) {
+    name = name || "Hero";
+      this.name = name;
+      this.position = '00';
+      this.health = 100;
+      this.damage = 5;
+      this.experience = 0;
+  
+  }
