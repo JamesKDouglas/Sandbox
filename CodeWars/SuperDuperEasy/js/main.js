35d758b620983ac7bfe812c711a01a88e6761b97
James Douglas <james@phytochemconsulting.ca>
2023-06-14 16:42:52 -0700

    add SuperDuperEasy

---
+++ CodeWars/SuperDuperEasy/js/main.js
@@ -0,0 +1,27 @@
+//Input: integer expected. If it is a string return "Error".
+
+//Our goal is to multily the integer by 50 and add 6.
+
+//BigInts? Nope.
+//Boolean? Not expected.
+
+//ex: 
+//1 => 56
+//"a" => Error
+//5 => 256
+
+function problem(x){
+    console.log(x);
+    if (typeof(x) === "string"){
+      return "Error";
+    } else {
+      return x*50+6;
+    }
+  }
+  
+  console.log(problem(1), 56);
+  console.log(problem("a"), "Error");
+  console.log(problem(5), 256);
+  console.log(problem(-3), -144);
+  
+  console.log(problem(""), "Error");
