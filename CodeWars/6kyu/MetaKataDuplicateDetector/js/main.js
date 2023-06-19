bac87a28f3a87b02b21abbf2fda3087ff00a8d21
James Douglas <james@phytochemconsulting.ca>
2023-06-19 12:02:00 -0700

    keep going on MetKatDuplicateDetector

--- CodeWars/6kyu/MetaKataDuplicateDetector/js/main.js
+++ CodeWars/6kyu/MetaKataDuplicateDetector/js/main.js
@@ -36,21 +36,64 @@
 
 
 function dupeDetect(arr){
-	//nested for loops?
-  //create a 2D array which has the function and the outputs from 0 to 9 input.
-  //Then scan horizontally across that 2D array to find duplicates.
-  console.log(arr);
-  let resultsArr = [new Array(arr.length)];
-//   console.log(arr[0](0));
-  for (let i=0;i<9;i++){//input
+  //We'll use nested for loops to generate the outcomes.
+
+  //Then examine the outcomes for repeats.
+
+  //so first I'll create a 2D array using the functions
+  // and the outputs that result from a 0 to 9 input.
+
+  //look for duplicates in each line of that 2D array. A line will be for an input: 0, 1, 2, etc.
+
+  //And make a new array that contains the indexes of identical outputs.
+
+  //ex: 0 to 9 with the example above generates,
+// [0, 0, 20, 0, 0, 0, 0]
+// [2, 1, 21, 0.001, 1, 1, 1]
+// [4, 4, 22, 0.002, 4, 4, 0]
+// [6, 9, 23, 0.003, 9, 9, 1]
+// [8, 16, 24, 0.004, 16, 16, 0]
+// [10, 25, 25, 0.005, 25, 25, 1]
+// [12, 36, 26, 0.006, 36, 36, 0]
+// [14, 49, 27, 0.007, 49, 49, 1]
+// [16, 64, 28, 0.008, 64, 64, 0]
+
+//Then the repeats array will be:
+// [0,1,3,4,5,6]
+// [1,4,5,6]
+// [1,4,5]
+// [1,4,5]
+// [1,4,5]
+// [1,2,4,5]
+// [1,4,5]
+// [1,4,5]
+// [1,4,5]
+
+//from this we are supposed to report [1,4,5]. Of course we can't truly know if this is ever
+//a coincidence or not by just trying values. But we do see is that 1,4,5 is in every single one
+//of the repeat arrays.
+//So, I guess take the shortest and check to see if all the other ones that are longer contain
+//those values. use .includes.
+
+  let resultsArr = [[]];
+  for (let i=0;i<=9;i++){//input
+    resultsArr.push([]);//create a blank element we can use;
     for (let j=0;j<arr.length;j++){//function
-        console.log("function:", resultsArr);
-      resultsArr[j].push[arr[j](i)];
-      console.log("push", arr[j](i));
-      console.log(resultsArr);
+    //   console.log("j is: ", j);
+    //   console.log("function:", arr[j]);
+    //   console.log("push", arr[j](i));
+    //   console.log("resultsArr: ",resultsArr);
+      resultsArr[i].push(arr[j](i));
     }
   }
-  console.log(resultsArr);
+  resultsArr.pop();//take off the extra empty array
+
+  //generate repeats array
+  //Really what that means is that if a number appears twice, the index gets added.
+  
+
+  //find the set of numbers that appears in every single repeats array entry.
+
 }
 
 const functionList = [
