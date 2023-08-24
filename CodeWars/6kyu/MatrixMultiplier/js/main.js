d708a7471c9ec1087f66bb23acc1888ffe59c628
James Douglas <james@phytochemconsulting.ca>
2023-08-24 13:15:41 -0700

    add matrixMultiplier

--- CodeWars/6kyu/MatrixMultiplier/js/main.js
+++ CodeWars/6kyu/MatrixMultiplier/js/main.js
@@ -21,5 +21,68 @@
 
 
 function getMatrixProduct(a, b) {
-  //If a and b can be multiplied, returns the product of a and b as a two-dimensional array. Otherwise returns null.
+//first, check the dimensions
+//a is supposed to be multipled by b so the first element of a should have the same
+//number of elements as b has.
+
+//width a
+let wa = a[0].length;
+//height b
+let hb = b.length;
+if (wa !== hb){
+  console.log("dimensions if the arrays don't match!")
+  return null;
+}
+
+//on to multiplication.
+//a for loop for the construction of the new array.
+//Then counters with modulo to keep track of what to actually do.
+
+//I'm going to count where I'm at for calculating. column counter a...
+let cca = rcb = 0;
+let sum = 0;
+let dotProd = [];
+for (let i=0;i<a.length;i++){
+  dotProd.push([]);
+}
+
+//First we combine the row 0 with column 0. Then row 1 with column 0
+//After we're done with all the rows of a we move on to using column 1.
+//So the outter loop is ccb
+
+for (ccb=0; ccb<b[0].length;ccb++){
+  for (let rca=0;rca<a.length;rca++){
+    // we've chosen our column and row. now create sum
+    sum = 0;
+
+    while (cca<a[0].length) {
+      // console.log("a[0].length",  a[0].length);
+      // console.log("first term a[rca][cca]",a[rca][cca]);
+      // console.log("second term",b[rcb][ccb]);
+      // console.log("new product:",a[rca][cca]*b[rcb][ccb]);
+      // console.log("counters rca, cca, rcb, ccb", rca, cca, rcb, ccb);
+      sum += a[rca][cca]*b[rcb][ccb];
+      rcb++;
+      cca++;
+    } 
+    // console.log(dotProd);
+    // console.log("end a sum. will push ", sum);
+    // console.log("counters rca, cca, rcb, ccb", rca, cca, rcb, ccb);
+    //push sum
+    dotProd[rca][ccb] = sum;
+    // console.log(dotProd);
+    rcb=0;
+    cca=0;
+  }
+}
+return dotProd;
+
 };
+
+// console.log(getMatrixProduct([[1, 2], [3, 4]], [[5, 6], [7, 8]]), [[19, 22], [43, 50]]);
+
+let arr1 =  [ [ 7, 3 ], [ 2, 5 ], [ 6, 8 ], [ 9, 0 ] ];
+let arr2 =  [ [ 7, 4, 9 ], [ 8, 1, 5 ] ];
+console.log(getMatrixProduct(arr1, arr2), [ [ 73, 31, 78 ], [ 54, 13, 43 ], [ 106, 32, 94 ], [ 63, 36, 81 ] ]);
+
+// [ [ 73, 31, 78, NaN ], [ 54, 13, 43, NaN ], [ 106, 32, 94, NaN ], [ 63, 36, 81, NaN ] ] to deeply equal [ [ 73, 31, 78 ], [ 54, 13, 43 ], [ 106, 32, 94 ], [ 63, 36, 81 ] ]
