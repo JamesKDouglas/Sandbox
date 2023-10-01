c4cd0660500c1069594aaf471a69db9c2a249be0
James Douglas <james@phytochemconsulting.ca>
2023-10-01 10:50:05 -0700

    all done Lucas_Lehmer's Mersenne Prime thingy

--- CodeWars/6kyu/Lucas_Lehmer/js/main.js
+++ CodeWars/6kyu/Lucas_Lehmer/js/main.js
@@ -4,20 +4,24 @@
 
 //Mp = 2^p -1
 
-//This gives you the Mersenne series. Inside that there are a bunch of prime numbers.
+//This gives you the Mersenne series if we input p as 1, 2, 3, etc.
+//Inside that there are a bunch of prime numbers.
 //That set is the Mersenne Primes.
 
-//So we can predict that series
+//We also know that we can easily test to see if a Mersenne number is prime. 
 
-//We also know that we can test to see if it's prime. 
-//For mersenne numbers there is a prime test:
+//For Mersenne numbers there is a prime test:
 //if S(p-1) = 0 mod Mp then it's prime.
-//0 mod Mp means the S(p-1) is divisible by the mersenne number.
+
+//The mod notation here is written incorrectly.
+// 0 mod Mp is just zero. Divide 0 by Mp and there is a remainder of 0.
+
+//Checking the reference, what they mean is 0 (mod Mp). That is, S(p-1) is evenly
+//divisibly by Mp.
 
 //The S series is:
 //S(n+1)= S(n)^2 -2
 //with S(1) = 4;
-
 
 //return a boolean.
 
@@ -27,26 +31,33 @@
 //input will be valid.
 
 lucas_lehmer = function(p) {
-  console.log("p:", p);
+
   //p into bigInt
   //Ok CodeWars can't use BigInt because it has node under 10.4
   //Declare everything else in bigInt that we will need. All the variable equations.
-  let m = 1;
-  let sFirst = 4;
-  let sSecond = 1;
+  let m = 1n;
+  let sFirst = 4n;
+  let sSecond = 1n;
+  let pBI = BigInt(p);
+  console.log("p big int:", pBI);
+
+  
 
   //Generate Mp
   //Generate S series to find S(p-1)
   //Perform modulo test using S(p-1) and Mp.
-  m = 2**p - 1;
-  
-  for (let i=1;i<p-1;i++){
-    sSecond = sFirst**2 - 2;
+  m = 2n**pBI - 1n;
+  // console.log("Mp:", m);
+
+  for (let i=1n;i<pBI-1n;i=i+1n){
+    // console.log("sFirst:", sFirst);
+    sSecond = sFirst**2n - 2n;
     sFirst = sSecond;
   }
-  console.log("sFirst:", sFirst);
+// console.log("S(p-1):", sFirst);
   
-  if (sFirst%m === 0){
+// console.log("sFirst%m:", sFirst%m);
+  if (sFirst%m === 0n){
     return true;
   } else {
     return false;
@@ -54,6 +65,12 @@ lucas_lehmer = function(p) {
   
 }
 
-console.log(lucas_lehmer(2), true);
+// console.log(lucas_lehmer(2), true);
+//Well the unput is p. The prime test only holds if p is an odd prime. 2 is not an odd prime.
+
 console.log(lucas_lehmer(7), true);
 console.log(lucas_lehmer(10), false);
+
+//Ok so one of the problems here is that we need to use bigInt.
+// But BigInt was only introduced into Node 10.4. And codewars doesn't let us use that.
+//So I can do this problem locally but not on Codewars!
