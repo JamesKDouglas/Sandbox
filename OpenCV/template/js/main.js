74c66224a9f0d8b57eec1d633631ba36fdfe699c
James Douglas <james@phytochemconsulting.ca>
2023-12-18 20:51:35 -0800

    add an image processing script to generate ndvi from a hypercube using python

--- OpenCV/template/js/main.js
+++ OpenCV/template/js/main.js
@@ -6,21 +6,24 @@ inputElement.addEventListener('change', (e) => {
 }, false);
 
 imgElement.onload = function() {
+  console.log("about to make a mat!")
   let mat = cv.imread(imgElement);
-
+  console.log("file read and made into a mat!")
   //Now the image file is imported in a standard format. We can process it. 
 
-  // let grayImg = new cv.Mat();
+  // let grayImg = new cv.Mat();//make an empty object to hold the output
+
   // cv.cvtColor(mat, grayImg, cv.COLOR_RGBA2GRAY, 0);
 
-  let dst = new cv.Mat();
+  // let dst = new cv.Mat();
 
-  let low = new cv.Mat(mat.rows, mat.cols, mat.type(), [0, 0, 0, 0]);
-  let high = new cv.Mat(mat.rows, mat.cols, mat.type(), [150, 150, 150, 255]);
-  cv.inRange(mat, low, high, dst);
-  cv.imshow('canvasOutput', dst);
-  mat.delete(); 
-  dst.delete();
+  // let low = new cv.Mat(mat.rows, mat.cols, mat.type(), [0, 0, 0, 0]);
+  // let high = new cv.Mat(mat.rows, mat.cols, mat.type(), [150, 150, 150, 255]);
+  
+  // cv.inRange(mat, low, high, dst);
+  // cv.imshow('canvasOutput', dst);
+  // mat.delete(); 
+  // dst.delete();
   
   // cv.imshow('canvasOutput', grayImg);
 };
