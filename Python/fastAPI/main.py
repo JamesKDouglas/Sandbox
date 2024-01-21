b397c32a0f0fe19cb4d3236597d760cfa0af99b9
James Douglas <james@phytochemconsulting.ca>
2024-01-20 19:23:53 -0800

    able to upload jpgs, convert them to numpy array and collect radio button info from form to decide which filter to apply

--- Python/fastAPI/main.py
+++ Python/fastAPI/main.py
@@ -8,29 +8,51 @@
 # when you make a change to a file it will reload the server.
 
 from fastapi.staticfiles import StaticFiles
-from fastapi import FastAPI, UploadFile, File
-from PIL import Image
-from io import BytesIO
+from fastapi import FastAPI, UploadFile, Form, Depends
 import shutil as shutil
 
+# for numpy arrays
+from PIL import Image
+from io import BytesIO
+import numpy as np
+import copy
+
+# this helps process the POST request body to include radio button info
+from dataclasses import dataclass
+@dataclass
+class FilterMyPhoto:
+    filter: str = Form(...) 
+    file: UploadFile = Form(...)
+
+# On with the main bit:
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+def saveAsNumpy(data):
+    image = read_imagefile(data.file.read())
+    np.save("./files/" + data.filename, image)
+    return
 
 def read_imagefile(data) -> Image.Image:
     image = Image.open(BytesIO(data))
     return image
 
 @app.post("/uploadfile/")
-async def read_root(file: UploadFile = File(...)):
-    with open("./files/"+file.filename, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+async def read_root(reqBody: FilterMyPhoto = Depends()):
     
-    return {"filename": file.filename}
+    photo = reqBody.file
+    photo2 = copy.deepcopy(photo)
 
-    # if you want the file as a numpy array do this:
+    # this reading as a numpy array seems to mess up the saving as a jpeg. Why can't they coexist?
 
-    # image = read_imagefile(await file.read())
+    saveAsNumpy(photo2)
+    
+    with open("./files/"+photo.filename, "wb") as buffer:
+        shutil.copyfileobj(photo.file, buffer)
 
-    # process it, or save it for later:
-    # np.save("./files/" + file.filename, image)
+
+    # now for filtering the image, refer to reqBody.filter. It can be "bw" or "red" right now.
+    # should I use OpenCV for the filtering? idk, whatever works. OpenCV seems like overkill
+    
+    return {"filter": reqBody.filter}
