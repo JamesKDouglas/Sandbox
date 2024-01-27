3fd353d6b7a37babe8d0b7ad9a7be621b6beafea
James Douglas <james@phytochemconsulting.ca>
2024-01-26 17:28:11 -0800

    more troubleshooting

--- Python/fastAPI/main.py
+++ Python/fastAPI/main.py
@@ -1,51 +1,97 @@
-# made by chatGPT. This is the third try. The first time it tried to use FileResponse with bytesIO, which is not allowed:
-# The second time, it mostly worked but the file does not appear in the browser as it should.
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse, StreamingResponse
+# This is a version of code that succfully returns a file. I took it from the previous commit
+
+# to make a basic fastAPI server all you do is
+# make a virtual environment with python -m venv .venv
+# activate the environment with source activate
+# import the dependencies with pip:
+# fastapi, "uvicorn[standard]", multipart (for file uploading)
+# make a main.py like below
+# run it with uvicorn main:app --reload from the directory where main.py is
+# when you make a change to a file it will reload the server.
+
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, UploadFile, Query, Form, Depends
+import shutil as shutil
+
+import cv2
+# import cv
+
+from fastapi.responses import FileResponse
+
+# for numpy arrays
 from PIL import Image
 from io import BytesIO
-import os
-from fastapi.staticfiles import StaticFiles
+import numpy as np
 
+
+# this helps process the POST request body to include radio button info
+from dataclasses import dataclass
+@dataclass
+class FilterMyPhoto:
+    filter: str = Form(...) 
+    file: UploadFile = Form(...)
+
+# On with the main bit:
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-def process_image(file_content: bytes) -> bytes:
-    # Open the image using PIL
-    image = Image.open(BytesIO(file_content))
+def saveAsNumpy(data):
+    image = read_imagefile(data.file.read())
+    np.save("./files/" + data.filename, image)
+    # after reading a file remember to close it if you want to read it again!
+    data.file.seek(0)
+    return
 
-    # Convert the image to grayscale
-    grayscale_image = image.convert('L')
+def read_imagefile(data) -> Image.Image:
+    image = Image.open(BytesIO(data))
+    return image
 
-    # Save the processed image to a BytesIO object
-    processed_image_content = BytesIO()
-    grayscale_image.save(processed_image_content, format="JPEG")
-
-    return processed_image_content.getvalue()
+def adjust_contrast_brightness(img, contrast:float=1.0, brightness:int=0):
+    """
+    Adjusts contrast and brightness of an uint8 image.
+    contrast:   (0.0,  inf) with 1.0 leaving the contrast as is
+    brightness: [-255, 255] with 0 leaving the brightness as is
+    """
+    brightness += int(round(255*(1-contrast)/2))
+    return cv2.addWeighted(img, contrast, img, 0, brightness)
 
 @app.post("/uploadfile/")
-async def upload_and_process_image(file: UploadFile = File(...)):
-    # Read the uploaded image content
-    file_content = await file.read()
+async def read_root(reqBody: FilterMyPhoto = Depends()):
+    
+    photo = reqBody.file
 
-    # Process the image to grayscale
-    processed_image_content = process_image(file_content)
+    fileNameOriginal = "./files/"+photo.filename
+    
+    saveAsNumpy(photo)
 
-    # Save the processed image to a temporary file
-    temp_filename = "/tmp/processed_image.jpg"  # Use an appropriate temporary directory
-    with open(temp_filename, "wb") as temp_file:
-        temp_file.write(processed_image_content)
+    with open(fileNameOriginal, "wb") as buffer:
+        shutil.copyfileobj(photo.file, buffer)
 
-    # Set additional headers
-    headers = {
-        "Content-Disposition": f"inline; filename=processed_image.jpg",
-        "Cache-Control": "no-cache, no-store, must-revalidate",
-        "Pragma": "no-cache",
-        "Expires": "0",
-    }
 
-    # Return the processed image as a StreamingResponse with headers
-    print(temp_filename)
-    return StreamingResponse(open(temp_filename, "rb"), media_type="image/jpeg", headers=headers) 
+    if reqBody.filter == "bw":
+        # im_gray = cv2.imread(fileNameOriginal, cv2.IMREAD_GRAYSCALE)
+        # (thresh, im_bw) = cv2.threshold(im_gray, 255, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        
+        # image = cv2.imread(fileNameOriginal) 
+        # gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+        # cv2.imwrite("./files/"+"f_"+ photo.filename, gray_image)
+        # return FileResponse("./files/"+"f_"+photo.filename)
 
+        image = cv2.imread(fileNameOriginal) 
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+
+        # "sharpen"
+        # kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+        # image = cv2.filter2D(image, -1, kernel)
+
+        image = adjust_contrast_brightness(image,1.25,0)
+        filename = "./files/"+"f_" + "br" + photo.filename
+        cv2.imwrite(filename, image)
+        print(filename)
+        return FileResponse(filename)
+
+    # now for filtering the image, refer to reqBody.filter. It can be "bw" or "red" right now.
+    # should I use OpenCV for the filtering? idk, whatever works. OpenCV seems like overkill
+    
+    
--- Python/fastAPI/main4.py
+++
@@ -1,1 +0,0 @@
-# This is a version of code that succfully returns a file. I took it from the previous commit
